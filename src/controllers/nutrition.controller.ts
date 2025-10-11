2215a0ae0a1a8cb61e2bcd42bcba2908072cc433
01kevinms <kevin.victors29@gmail.com>
2025-10-11 18:53:54 -0300

    corrects

--- src/controllers/nutrition.controller.ts
+++ src/controllers/nutrition.controller.ts
@@ -1,5 +1,5 @@
  import { FastifyRequest, FastifyReply } from "fastify"; 
- import fetch from "node-fetch"; 
+ import fetch from "node-fetch";
  import { NutritionixFood } from "../types/fastify"; 
 
 
